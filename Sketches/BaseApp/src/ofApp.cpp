962bb210acdffee160ce8a3dfa31ac3f1db335e7
Mark van de Korput <markkorput@localprojects.com>
2015-01-10 20:36:22 -0500

    add ofxSuperLog, ofxTimeMeasurements and ofxRemoteUI addons

--- Sketches/BaseApp/src/ofApp.cpp
+++ Sketches/BaseApp/src/ofApp.cpp
@@ -1,18 +1,59 @@
 #include "ofApp.h"
 
+#include "ofxSuperLog.h"
+#include "ofxTimeMeasurements.h"
+#include "ofxRemoteUIVars.h"
+
+//--------------------------------------------------------------
+// callback function for ofxRemoteUI actions
+bool bRecalcNow = true;
+
+void serverCallback(RemoteUIServerCallBackArg arg){
+    switch (arg.action) {
+        case CLIENT_DID_SET_PRESET:
+        case CLIENT_UPDATED_PARAM:
+            bRecalcNow = true;
+            break;
+        default:
+            break;
+    }
+}
+
 //--------------------------------------------------------------
 void ofApp::setup(){
     ofLogToFile("log.txt", true);
+    //ofSetLogLevel(OF_LOG_VERBOSE);
+
+    ofSetFrameRate(60);
+    ofSetVerticalSync(true);
+    //ofEnableAlphaBlending();
+    
+    // setup ofxRemoteUI
+    OFX_REMOTEUI_SERVER_SET_CALLBACK(serverCallback);
+    OFX_REMOTEUI_SERVER_SETUP(); 	//start server
+    TIME_SAMPLE_SET_FRAMERATE(60);
+    // TODO: setup params here
+    OFX_REMOTEUI_SERVER_LOAD_FROM_XML();
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+    TS_START("Update");
 
+    float dt = (1.f/60.f); // elapsed time (60 fps)
+
+    if(bRecalcNow){
+        // TODO: perform post-param-change updates
+    }
+    
+    TS_STOP("Update");
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    TS_START("Draw");
 
+    TS_STOP("Draw");
 }
 
 //--------------------------------------------------------------
