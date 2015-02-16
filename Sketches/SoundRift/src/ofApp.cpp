1b8eab7fc68c72a5cc7c3b49c40f6fccc836ceb5
Mark van de Korput <markkorput@localprojects.com>
2015-02-16 15:58:10 +0100

    new sketch with ofxOculusDK2 addon, builds

---
+++ Sketches/SoundRift/src/ofApp.cpp
@@ -0,0 +1,111 @@
+#include "ofApp.h"
+
+#include "ofxSuperLog.h"
+#include "ofxTimeMeasurements.h"
+#include "ofxRemoteUIVars.h"
+
+//--------------------------------------------------------------
+// callback function for ofxRemoteUI actions
+bool bRecalcNow = true;
+
+void ruiServerCallback(RemoteUIServerCallBackArg arg){
+    switch (arg.action) {
+        case CLIENT_DID_SET_PRESET:
+        case CLIENT_UPDATED_PARAM:
+            bRecalcNow = true;
+            break;
+        default:
+            break;
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofLogToFile("log.txt", true);
+    //ofSetLogLevel(OF_LOG_VERBOSE);
+
+    ofSetFrameRate(60);
+    ofSetVerticalSync(true);
+    ofEnableAlphaBlending();
+
+    TIME_SAMPLE_SET_FRAMERATE(60);
+    // TIME_SAMPLE_ENABLE();
+
+    // setup ofxRemoteUI
+    RUI_SETUP();
+    RUI_SET_CALLBACK(ruiServerCallback);
+    // TODO: setup params here
+    RUI_NEW_GROUP("App");
+    RUI_DEFINE_VAR_WV(bool, "app-fullscreen", false);
+    RUI_LOAD_FROM_XML();
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    float dt = (1.f/60.f); // elapsed time (60 fps)
+
+    if(bRecalcNow){
+        // TODO: perform post-param-change updates
+        ofSetFullscreen(RUI_VAR(bool, "app-fullscreen"));
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofBackgroundGradient(ofColor(120), ofColor(170));
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    switch(key){
+        case 'f':
+            OFX_REMOTEUI_SERVER_SET_VAR(bool, "app-fullscreen", !RUI_VAR(bool, "app-fullscreen"));
+            bRecalcNow = true;
+            break;
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::exit(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){
+	
+}
